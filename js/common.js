f4ee47b3a3a10ad159369958b3055dfb0c860a41
zhangzhenzhen073 <usernam736601900@qq.com>
2017-12-01 22:59:25 +0800

    xg

--- js/common.js
+++ js/common.js
@@ -10,89 +10,40 @@ function refreshRem(){
 refreshRem();
 window.addEventListener('resize',refreshRem);
 
-/*圆形进度条 myCanvas*/
-var c=document.getElementById('myCanvas');
-var ctx=c.getContext('2d');
-var x=30;
-//灰色的圆
-//开始一条路径
-ctx.beginPath();
-//设置当前线条的宽度
-ctx.lineWidth = 6; //10px
-//设置线条的颜色
-ctx.strokeStyle = '#e5e5e5';
-ctx.arc(27, 27, 23, 0, 2 * Math.PI);
-//绘制已定义的路径
-ctx.stroke();
-//百分比所占的粉色的圆
-ctx.beginPath();
-ctx.lineWidth = 6;
-ctx.strokeStyle = '#fb0065';
-//-90度为起点 弧度为-0.5*Math.PI
-//百分比如26% 所占的弧度为 26/100*Math.PI*2
-ctx.arc(27, 27, 23, -0.5*Math.PI,x/100*Math.PI*2-0.5*Math.PI);
-ctx.stroke();
-//圆里面的文字
-ctx.font = '14px Arial';
-ctx.fillStyle = '#333';
-ctx.textBaseline = 'middle';
-ctx.textAlign = 'center';
-ctx.fillText(x+'%', 27, 27);
 
-/*圆形进度条 myCanvas*/
-var c=document.getElementById('myCanvas01');
-var ctx=c.getContext('2d');
-var x=40;
-//灰色的圆
-//开始一条路径
-ctx.beginPath();
-//设置当前线条的宽度
-ctx.lineWidth = 6; //10px
-//设置线条的颜色
-ctx.strokeStyle = '#e5e5e5';
-ctx.arc(27, 27, 23, 0, 2 * Math.PI);
-//绘制已定义的路径
-ctx.stroke();
-//百分比所占的粉色的圆
-ctx.beginPath();
-ctx.lineWidth = 6;
-ctx.strokeStyle = '#fb0065';
-//-90度为起点 弧度为-0.5*Math.PI
-//百分比如26% 所占的弧度为 26/100*Math.PI*2
-ctx.arc(27, 27, 23, -0.5*Math.PI,x/100*Math.PI*2-0.5*Math.PI);
-ctx.stroke();
-//圆里面的文字
-ctx.font = '14px Arial';
-ctx.fillStyle = '#333';
-ctx.textBaseline = 'middle';
-ctx.textAlign = 'center';
-ctx.fillText(x+'%', 27, 27);
+drawCircle =  function(canvas,number){
+    var  clientWidth = document.documentElement.clientWidth;
+    var canvasWidth = Math.floor(clientWidth*121/750);
+    var innerR = canvasWidth * 0.75*0.5;
+    var ctx = canvas.getContext('2d');
+    canvas.setAttribute('width',canvasWidth+'px');
+    canvas.setAttribute('height',canvasWidth+'px');
+    ctx.translate(canvasWidth/2,canvasWidth/2);
+    //绘制灰色的圆
+    ctx.beginPath();
+    ctx.arc(0, 0, innerR, 0, Math.PI*2,false);
+    ctx.lineWidth = 6;
+    ctx.strokeStyle = "#e5e5e5";
+    ctx.stroke();
+    //绘制粉色的圆
+    ctx.beginPath();
+    ctx.arc(0, 0, innerR, -0.5*Math.PI,number/100*Math.PI*2-0.5*Math.PI,false);
+    ctx.lineWidth = 6;
+    ctx.strokeStyle = "#fb0065";
+    ctx.stroke();
+    // 设置字体
+    ctx.font = "24px";
+    // 设置对齐方式
+    ctx.textAlign = "center";
+    // 设置填充颜色
+    ctx.fillStyle = "#333";
+    // 设置字体内容，以及在画布上的位置
+    ctx.fillText(number+'%', 0, 2);
+};
+drawCircle(document.getElementById('myCanvas'),30);
+drawCircle(document.getElementById('myCanvas01'),50);
+drawCircle(document.getElementById('myCanvas02'),80);
 
-/*圆形进度条 myCanvas*/
-var c=document.getElementById('myCanvas02');
-var ctx=c.getContext('2d');
-var x=10;
-//灰色的圆
-//开始一条路径
-ctx.beginPath();
-//设置当前线条的宽度
-ctx.lineWidth = 6; //10px
-//设置线条的颜色
-ctx.strokeStyle = '#e5e5e5';
-ctx.arc(27, 27, 23, 0, 2 * Math.PI);
-//绘制已定义的路径
-ctx.stroke();
-//百分比所占的粉色的圆
-ctx.beginPath();
-ctx.lineWidth = 6;
-ctx.strokeStyle = '#fb0065';
-//-90度为起点 弧度为-0.5*Math.PI
-//百分比如26% 所占的弧度为 26/100*Math.PI*2
-ctx.arc(27, 27, 23, -0.5*Math.PI,x/100*Math.PI*2-0.5*Math.PI);
-ctx.stroke();
-//圆里面的文字
-ctx.font = '14px Arial';
-ctx.fillStyle = '#333';
-ctx.textBaseline = 'middle';
-ctx.textAlign = 'center';
-ctx.fillText(x+'%', 27, 27);
+
+
+
